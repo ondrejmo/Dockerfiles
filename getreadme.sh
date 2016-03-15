2df8e7f4691f2474dec8467507f463d54659450e
Ondřej Molík <ondrejmo@protonmail.ch>
2016-03-15 20:21:47 +0100

    better getreadme.sh

--- getreadme.sh
+++ getreadme.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ## Export README.md label file from image
 getreadme(){
-  echo "This function will work only against ondrejmo/* docker images."
+  echo "This function will work only against ondrejmo/* docker images." | lolcat
   sleep 1
   if [ "$(echo $1 | head -c 8)" != "ondrejmo" ]; then
-    echo "This image isn't supported";
+    echo "This image isn't supported" | lolcat;
   else
     NAME=${1##*/}
-    docker run --name $NAME $1 /bin/true
-    docker cp $NAME:/README.md $NAME.md
-    docker rm $NAME;
+    docker run --name "$NAME" "$1" /bin/true
+    docker cp "$NAME":/README.md "$NAME".md
+    docker rm "$NAME";
   fi
 }
